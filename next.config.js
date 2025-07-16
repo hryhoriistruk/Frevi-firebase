dcb5f8433155b06fe876f1ae9edc1bbb05b1cbb1
jnjijij <strukhr@gmail.com>
2025-07-16 11:34:44 +0200

    т

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // output: 'export',
-    distDir: 'out',
-    // trailingSlash: true,
-    output: 'standalone',
+    output: 'export',
+    trailingSlash: true,
     images: {
         unoptimized: true
     },
