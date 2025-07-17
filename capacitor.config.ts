21c5740fe4dc390621951a59026ead9576e993aa
jnjijij <strukhr@gmail.com>
2025-07-17 22:18:25 +0200

    лт

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.frevi.app',
+  appId: 'com.example.app',
   appName: 'frevi',
   webDir: 'public'
 };
