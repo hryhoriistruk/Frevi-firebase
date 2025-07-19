0f9d42754061e1dc791fbf581722c84d51e7d4e1
jnjijij <strukhr@gmail.com>
2025-07-19 20:25:50 +0200

    l

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export', // Uncomment if you need static export
+    // output: 'export', // Uncomment if you need static export
     trailingSlash: true,
     images: {
         unoptimized: true
