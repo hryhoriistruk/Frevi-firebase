dbf26e2d8fc1e485e6d8e5fc119552fb9126d370
jnjijij <strukhr@gmail.com>
2025-07-31 21:48:18 +0200

    k

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // output: 'export',
+    output: 'export',
     distDir: 'out',
     trailingSlash: true,
     serverExternalPackages: ['openai'],
@@ -52,7 +52,7 @@ module.exports = {
     },
 }
 module.exports = {
-    // output: 'export', // Для генерації статичних файлів
+    output: 'export', // Для генерації статичних файлів
     distDir: 'out',  // Папка для збірки
     images: {
         unoptimized: true // Вимкнути оптимізацію зображень
